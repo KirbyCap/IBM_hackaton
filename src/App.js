e9233a5707ac935519da4ceadeaa2fbdba2e9215
k-will22 <thestreetkw@hotmail.com>
2021-09-07 15:37:22 -0400

    css 4

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
   
   return (
     <div className="App">
-      <h1 className="title"><img src={"./logo3.png"} width="100px" height="35px"></img> Hackathon</h1>
+      <h1 className="title"><img src={"./logo4.png"} width="100px" height="35px"></img> Hackathon</h1>
       <div className="border">
       <h2 className="team">TEAM B</h2>
       <p className="names">Maxwell Hostetter, Kirby Chen, Keith Williams, Timmy Roma</p>
